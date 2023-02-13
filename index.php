640d0170a732db8969d60401f8bcd64db7d218db
su apt install Kuma <maxxozou@gmail.com>
2023-02-13 09:59:21 +0100

    Ver 1.1

--- index.php
+++ index.php
@@ -5,11 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="style/login_signup.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"rel="stylesheet">
+    <link rel="stylesheet" href="style/index.css">
 
     <title>Document</title>
 </head>
 <body>
-    
+    <nav>
+        <div class="logo">
+            <span class="blue">Cool</span>Shopping
+        </div>
+
+        <div class="liens">
+            <div class="first"><a href="#"><span class="material-icons-round">grid_view</span>Tout les produits</a></div>
+            <div class="second"><a href="#"><span class="material-icons-round">card_giftcard</span>Carte Cadeaux</a></div>
+        </div>
+
+        <div class="search">
+            <span class="material-icons-round">search</span>
+            <input type="search" name="searchBar" placeholder="Faire une recherche">
+        </div>
+
+        <div class="profil">
+            <a href="#"><span class="material-icons-round">favorite_border</span></a>
+            <a href="#"><span class="material-icons-round">shopping_cart</span></a>
+            <a href="#"><img src="img/defaultAvatar/Avatar-1.png" alt="Avatar_user" title="Accéder à mon profil"></a>
+        </div>
+    </nav>
 </body>
 </html>
